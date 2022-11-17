0b75769aa6b86bebf2e299db374557f1953e8a43
Alexander Kulikovskiy <alexander.kulikovski@gmail.com>
2022-11-17 22:53:31 +0200

    refactor string generator

--- scripts/update_localization.sh
+++ scripts/update_localization.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-command="twine generate-all-localization-files"
+command_general="twine generate-all-localization-files"
+command="twine generate-localization-file"
 res_path="strings/strings.txt"
+res_output_path="androidPhoneScreen/src/main/res/values"
 
 declare -a platform_arguments=(
-  "androidPhoneScreen/src/main/res/ -r --format android"
   "iosApp/iosApp/LocalizedStrings --format apple"
 )
 
 for platform in "${platform_arguments[@]}"; do
-  ${command} ${res_path} ${platform}
+  ${command_general} ${res_path} ${platform}
 done
+
+#TODO add map path to locale
+${command2} ${res_path} ${res_output_path}/generated_strings.xml --lang en --format android
+${command2} ${res_path} ${res_output_path}-ru/generated_strings.xml --lang ru --format android
 
 # cd iosApp/
 # swiftgen
